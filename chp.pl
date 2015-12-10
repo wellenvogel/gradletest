d6d13c114d79d713f35898d8a283f26666a90113
andreas <andreas@wellenvogel.de>
2015-12-10 20:58:48 +0100

    monitor process

---
+++ chp.pl
@@ -0,0 +1,54 @@
+#! /usr/bin/env perl
+use strict;
+my $cmdpid=$ARGV[0];
+my $ppid=getppid();
+if ( $cmdpid <= 1) {
+	print STDERR "invalid pid on commandline: $cmdpid, exiting\n";
+	exit
+}
+if ($ppid <= 1){
+	print STDERR "invalid ppid $ppid, exiting\n";
+	exit
+}
+my $marker=$ENV{'MARKER'};
+if (  $marker eq "") {
+	print STDERR "marker MARKER not found in env\n";
+	exit
+}
+while (1){
+	my $rt=0;
+	if ($cmdpid == $ppid){
+		$rt=kill(0,$ppid);
+		print "parent $ppid status=$rt\n";
+	}
+	else{
+		print "parent pid $ppid does not match pid from commandline $cmdpid, cleaning up\n";
+	}
+	if ($rt <= 0){
+		print "PARENT $cmdpid exited, cleaning up\n";
+		for my $proc (glob("/proc/[0-9]*")){
+			my $pid=$proc;
+			$pid=~s?.*/??;
+			$/="\0";
+			my $ef=$proc."/environ";
+			if (-r $ef && $pid != $$ ){
+				open(my $h,"<",$ef);
+				if ($h){
+					while(<$h>){
+						chomp;
+						my ($n,$v)=split(/=/,$_,2);	
+						if ($n eq "MARKER" && $v eq $marker){
+							print "found marked process $pid, killing it\n";
+							kill 15,$pid;
+						}
+					}
+					close($h);
+				}
+			}
+		}
+		sleep(1);
+		print "monitor exiting\n";
+		exit(0);
+	}	
+	sleep(1)
+}
---
+++ test.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+MARKER=`dirname $0`$$
+export MARKER
+echo "MARKER=$MARKER"
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+sleep 100 &
+
+`dirname $0`/chp.pl $$ &
+#sleep 20
+#echo "done"
+
